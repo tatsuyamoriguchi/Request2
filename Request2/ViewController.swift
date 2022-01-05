634ebc2ab5e8c29b6a3f0c70b2112aeecf6ac567
Brian Moriguchi <brian@beckos.com>
2022-01-05 09:22:54 -0800

    Initial Commit

---
+++ Request2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Request2
+//
+//  Created by Tatsuya Moriguchi on 1/5/22.
+//  Copyright © 2022 Tatsuya Moriguchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
